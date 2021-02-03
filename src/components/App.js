dcd154245cad63068aa076f00cf164167cd969d4
Marmik Panchal <marmik.panchal@gmail.com>
2021-02-04 00:16:45 +1100

    Added actions and reducers

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import SongList from "./SongList";
+
+const App = () => {
+  return (
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column right wide">
+          <SongList />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
